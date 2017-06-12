3346d13c8591d1d0efdb7ae7cf4aa73c58614dc4
Chris Breiding <chrisbreiding@gmail.com>
2017-06-12 08:24:28 -0400

    simplify configuration

--- web/zunderfile.js
+++ web/zunderfile.js
@@ -1,7 +1,4 @@
 require('zunder').setConfig({
-  cacheBust: false,
-  devDir: 'app-dist',
-  prodDir: 'app-dist-prod',
   resolutions: ['react', 'react-dom', 'lodash'],
   staticGlobs: {
     'node_modules/font-awesome/fonts/**': '/fonts',
