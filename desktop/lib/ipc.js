5027c50df7474166ea3335465d0a051088601910
Chris Breiding <chrisbreiding@gmail.com>
2018-06-17 10:05:51 -0400

    clean up ipc module

--- desktop/lib/ipc.js
+++ desktop/lib/ipc.js
@@ -1,41 +1,16 @@
 'use strict'
 
-const _ = require('lodash')
 const { ipcMain } = require('electron')
-const Promise = require('bluebird')
 const window = require('./window')
-
-// const on = (requestName, callback) => {
-//   ipcMain.on(`${requestName}:request`, (event, ...args) => {
-//     callback((...reponseArgs) => {
-//       event.sender.send(`${requestName}:response`, ...reponseArgs)
-//     }, ...args)
-//   })
-// }
-
-// const once = ipcMain.once.bind(ipcMain)
-// const off = ipcMain.removeAllListeners.bind(ipcMain)
-
-// const request = (requestName, id, ...args) => {
-//   return window.ensure().then((win) => {
-//     return new Promise((resolve, reject) => {
-//       win.webContents.send(`${requestName}:request`, id, ...args)
-//       const suffix = id ? `:${id}` : ''
-//       ipcMain.once(`${requestName}:response${suffix}`, (event, error, response) => {
-//         if (error) {
-//           reject(_.extend(new Error(''), error))
-//         } else {
-//           resolve(response)
-//         }
-//       })
-//     })
-//   })
-// }
 
 const send = (eventName, ...args) => {
   return window.ensure().then((win) => {
     win.webContents.send(eventName, ...args)
   })
+}
+
+const sendDebug = (message) => {
+  return send('debug', message)
 }
 
 const sendInfo = (message, details) => {
@@ -51,12 +26,9 @@ const sendError = (message, err) => {
 }
 
 module.exports = {
-  // on,
-  // once,
-  // off,
-  // request,
   on: ipcMain.on.bind(ipcMain),
   send,
+  sendDebug,
   sendInfo,
   sendTitle,
   sendError,
