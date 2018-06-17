290bd2b1e41125ae747dfe8c95dfc8ef70648517
Chris Breiding <chrisbreiding@gmail.com>
2018-06-17 10:05:38 -0400

    send debug info about firebase creds

--- desktop/lib/remote-store.js
+++ desktop/lib/remote-store.js
@@ -2,6 +2,7 @@ const firebase = require('firebase')
 const Promise = require('bluebird')
 
 const db = require('./db')
+const ipc = require('./ipc')
 const { isDev } = require('./util')
 
 const account = isDev ? 'firebase-dev' : 'firebase'
@@ -9,6 +10,8 @@ const account = isDev ? 'firebase-dev' : 'firebase'
 class RemoteStore {
   start () {
     return db.fetchCredentials(account).then(({ appName, apiKey }) => {
+      ipc.sendDebug({ appName, apiKey })
+
       this._app = firebase.initializeApp({
         apiKey,
         authDomain: `${appName}.firebaseapp.com`,
